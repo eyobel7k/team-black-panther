15fd1438f95bd0d030380aad71bd656538b75fd4
alberm <albertmartinez16@gmail.com>
2021-10-29 16:06:11 -0700

    remove WPAPI calls

--- App.js
+++ App.js
@@ -1,14 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { posts, users } from './components/WPAPI';
+import { StyleSheet, View } from 'react-native';
+import Profile from './components/Profile';
 
 export default function App() {
-  posts();
-  users();
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Profile />
       <StatusBar style="auto" />
     </View>
   );
