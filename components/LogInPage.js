4c90954b026ec716c2106c84f40de19e1447b64e
Eyobel Legese <eyobel7k@gmail.com>
2021-10-29 16:38:15 -0700

    Removed the login and singup navigation.

--- components/LogInPage.js
+++ components/LogInPage.js
@@ -2,24 +2,11 @@ import React from "react";
 import { StyleSheet, Text, View, ScrollView } from "react-native";
 import HeaderLogPage from "./HeaderLogPage";
 import Footer from "./Footer";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import SignUpPage from "./SignUpPage";
 
 
 function LogInPage() {
-	const Stack = createNativeStackNavigator();
-
-	function Nav() {
-		return (
-			<NavigationContainer>
-				<Stack.Navigator initialRouteName="LogInPage">
-					<Stack.Screen name="LogInPage" component={LogInPage} />
-					<Stack.Screen name="SignUpPage" component={SignUpPage} />
-				</Stack.Navigator>
-			</NavigationContainer>
-		);
-	}
+	
 	return (
 		<View style={styles.container}>
 			<ScrollView>
