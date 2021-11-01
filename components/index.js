68281bc7c1090a8fb371673960580459cbc10940
alberm <albertmartinez16@gmail.com>
2021-11-01 15:25:06 -0700

    add index.js

---
+++ components/index.js
@@ -0,0 +1,25 @@
+import Footer from './Footer';
+import Friends from './Friends';
+import Header from './Header';
+import HeaderLogPage from './HeaderLogPage';
+import Home from './Home';
+import Images from './Images';
+import LogInPage from './LogInPage';
+import Newsfeed from './Newsfeed';
+import Profile from './Profile';
+import RestPasswordPage from './RestPasswordPage';
+import SignUpPage from './SignUpPage';
+
+export {
+  Footer,
+  Friends,
+  Header,
+  HeaderLogPage,
+  Home,
+  Images,
+  LogInPage,
+  Newsfeed,
+  Profile,
+  RestPasswordPage,
+  SignUpPage
+};
