ddce5d293a027dab9713de01a99d988c2fb2cf58
srosenberg74 <srosenberg@alphaworks.tech>
2021-11-02 16:41:14 -0700

    updated Newsfeed, renamed file to Newsfeed2

--- App.js
+++ App.js
@@ -1,30 +1,27 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet,View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { posts, users } from "./components/WPAPI";
 import Home from "./components/Home";
 import SignUpPage from "./components/SignUpPage";
 import LogInPage from "./components/LogInPage";
-
-
+import NewsFeed from "./components/Newsfeed2";
 
 export default function App() {
-	posts();
-	users();
-	return (
+  posts();
+  users();
+  return (
     <View style={styles.container}>
-      
-      <Home />
+      <NewsFeed />
       {/* <LogInPage/> */}
-      
-			<StatusBar style="auto" />
-		</View>
-	);
+
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-	
-	},
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
 });
---
+++ components/Newsfeed2.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, ScrollView, Button } from "react-native";
+import Header from "./Header";
+import Footer from "./Footer";
+import { posts } from "./WPAPI";
+import Post from "./Post";
+
+function Newsfeed() {
+  const [postsArr, setPostsArr] = useState([]);
+  useEffect(() => {
+    posts().then((data) => setPostsArr(data));
+  }, []);
+  const generatePosts = postsArr.map((post, i) => {
+    let content = post.excerpt.rendered;
+    content = content.replace("<p>", "");
+    content = content.replace("</p>", "");
+    content = content.replace("\n", "");
+    return <Post key={i} content={content} id={i} associatedContent={post} />;
+  });
+
+  return (
+    <View style={styles.body}>
+      <Text>Newsfeed</Text>
+      <ScrollView>{generatePosts}</ScrollView>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  body: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#fff",
+    height: "80%",
+    width: "100%",
+    textAlign: "center",
+    justifyContent: "center",
+  },
+  text: {
+    margin: 5,
+    fontSize: "1.5em",
+    fontWeight: 100,
+    fontFamily: "Serif",
+  },
+  post: {
+    border: "cadetBlue solid 2px",
+    backgroundColor: "aliceBlue",
+    margin: "1rem",
+    padding: "1rem",
+    borderRadius: "10px",
+    width: "40vw",
+  },
+});
+
+export default Newsfeed;
