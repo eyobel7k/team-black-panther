6929a8cba7ceb84029f092dfdd7a323d8df43154
alberm <albertmartinez16@gmail.com>
2021-11-02 14:22:44 -0700

    more work on wp api fetch

--- src/services/WPAPI.js
+++ src/services/WPAPI.js
@@ -1,13 +1,30 @@
 const BASE_URL = 'https://jualuc1.dreamhosters.com/wp-json';
+const WP_PATH = '/wp/v2';
+const BUDDYPRESS_PATH = '/buddypress/v1';
 
 export const WPAPI_PATHS = {
-  posts: `${BASE_URL}/wp/v2/posts`,
-  users: `${BASE_URL}/wp/v2/users`,
-  media: `${BASE_URL}/wp/v2/media`,
-  comments: `${BASE_URL}/wp/v2/comments`,
-  categories: `${BASE_URL}/wp/v2/catergories`,
-  blockTypes: `${BASE_URL}/wp/v2/block-types`,
-  blocks: `${BASE_URL}/wp/v2/blocks`,
+  wp: {
+    posts: `${BASE_URL}${WP_PATH}/posts`,
+    users: `${BASE_URL}${WP_PATH}/users`,
+    media: `${BASE_URL}${WP_PATH}/media`,
+    comments: `${BASE_URL}${WP_PATH}/comments`,
+    categories: `${BASE_URL}${WP_PATH}/catergories`,
+    search: `${BASE_URL}${WP_PATH}/search`,
+    blockTypes: `${BASE_URL}${WP_PATH}/block-types`,
+    blocks: `${BASE_URL}${WP_PATH}/blocks`,
+  },
+  buddypress: {
+    members: `${BASE_URL}${BUDDYPRESS_PATH}/members`,
+    activity: `${BASE_URL}${BUDDYPRESS_PATH}/activity`,
+  }
+}
+
+const urlBuilder = ( path, params = {} ) => {
+  let url = path;
+  for (let key in params) {
+    url += `${key}=${params[key]}`
+  }
+  return url;
 }
 
 /**
@@ -15,7 +32,7 @@ export const WPAPI_PATHS = {
  * @param {Promise} response
  * example use: wpApiFetch({ path: WPAPI_PATHS.posts }) returns Promise for wordpress posts array
  */
-export const wpApiFetch = async ({ path, data, method = 'GET' }) => {
+export const wpApiFetch = async ({ path, params, data, method = 'GET' }) => {
   try {
     const response = await fetch(path, { method, body: JSON.stringify(data) });
     return response.json();
@@ -25,6 +42,7 @@ export const wpApiFetch = async ({ path, data, method = 'GET' }) => {
   }
 }
 
+/** the old fetch functions */
 export const posts = () => {
   return fetch(`${BASE_URL}/wp/v2/posts`).then(response => response.json());
 }
