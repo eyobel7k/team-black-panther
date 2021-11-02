887bd4e9276b2481a525ea5bf9a66373fea4fa25
alberm <albertmartinez16@gmail.com>
2021-11-02 16:56:23 -0700

    edit styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,19 +3,15 @@ import { StyleSheet, Text, View, TouchableHighlight } from "react-native";
 
 function Header({ navigation }) {
 	return (
-		<View style={styles.Header} navigation={navigation}>
-			<TouchableHighlight style={styles.headerTouchableHighlight}>
+		<View style={styles.Header}>
+			<TouchableHighlight>
 				<View style={styles.HeaderTitle}>
 					<Text style={styles.headerText}>Marvelspace a space</Text>
 					<Text style={styles.headerText}>for super friends</Text>
 				</View>
 			</TouchableHighlight>
-			<TouchableHighlight
-				style={styles.headerTouchableHighlight}
-				navigation={navigation}
-			>
-				<View style={styles.HeaderBar} navigation={navigation}>
-					
+			<TouchableHighlight>
+				<View style={styles.HeaderBar}>	
 					<View>
 						<Text
 							style={styles.headerText}
@@ -58,7 +54,6 @@ function Header({ navigation }) {
 						>
 							Messages
 						</Text>
-					
 				</View>
 			</TouchableHighlight>
 		</View>
@@ -68,38 +63,25 @@ function Header({ navigation }) {
 const styles = StyleSheet.create({
 	Header: {
 		flex: 1,
-		Position: "absolute",
 		flexDirection: "column",
-		top: 0,
+		justifyContent: "space-between",
 		backgroundColor: "blue",
 		height: "4em",
 		width: "100%",
 	},
 	HeaderTitle: {
 		flexDirection: "column",
-		backgroundColor: "blue",
-		width: "100%",
 		justifyContent: "flex-start",
-		alignItems: "start",
-		padding: 10,
-		paddingBottom: 15,
+		alignItems: "flex-start",
+		padding: 30,
+		width: '100%',
 	},
 	HeaderBar: {
 		flexDirection: "row",
-		justifyContent: "flex-end",
-
 		backgroundColor: "skyblue",
 		width: "100%",
-		bottom: 0,
 		padding: 5,
 		justifyContent: "space-around",
-		alignItems: "start",
-	},
-
-	headerTouchableHighlight: {
-		alignItems: "center",
-		justifyContent: "center",
-		flex: 1,
 	},
 	headerText: {
 		color: "white",
