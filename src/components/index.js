6336a3f8842cefb12153a201f491728302730497
alberm <albertmartinez16@gmail.com>
2021-11-02 16:59:54 -0700

    add additional components

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+import EditProfile from './EditProfile';
 import Footer from './Footer';
 import Friends from './Friends';
 import Header from './Header';
@@ -5,12 +6,17 @@ import HeaderLogPage from './HeaderLogPage';
 import Home from './Home';
 import Images from './Images';
 import LogInPage from './LogInPage';
+import Message from './Message';
 import Newsfeed from './Newsfeed';
+import PasswordReset from './PasswordReset';
 import Profile from './Profile';
 import RestPasswordPage from './RestPasswordPage';
 import SignUpPage from './SignUpPage';
+import ThemeLoggedIn from './ThemeLoggedIn';
+import ThemeLoggedOut from './ThemeLoggedOut';
 
 export {
+  EditProfile,
   Footer,
   Friends,
   Header,
@@ -18,8 +24,12 @@ export {
   Home,
   Images,
   LogInPage,
+  Message,
   Newsfeed,
+  PasswordReset,
   Profile,
   RestPasswordPage,
-  SignUpPage
+  SignUpPage,
+  ThemeLoggedIn,
+  ThemeLoggedOut,
 };
