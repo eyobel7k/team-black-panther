21708fe1fb1821f93335540fd937849db7459772
Hiwot-Beshe <beshehiwot@gmail.com>
2021-11-02 14:15:39 -0700

    Add navigation, edit  profile page, and signup page

--- components/RestPasswordPage.js
+++ components/RestPasswordPage.js
@@ -1,48 +1,143 @@
-		import React from "react";
-		import { StyleSheet, Text, View, ScrollView } from "react-native";
-		import HeaderLogPage from "./HeaderLogPage";
-		import Footer from "./Footer";
+import React, { useState } from "react";
+import { Text } from "react-native-elements";
+import {
+	StyleSheet,
+	View,
+	TextInput,
+	Image,
+	Button,
+	TouchableOpacity,
+} from "react-native";
+import Footer from "./Footer";
+import HeaderLogPage from "./HeaderLogPage";
+function RestPasswordPage({ navigation }) {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
 
-		function RestPassWordPage() {
-			return (
-				<View style={styles.container}>
-					<ScrollView>
-						<View>
-							<HeaderLogPage />
-						</View>
+	return (
+		<View style={styles.container} navigation={navigation}>
+			<View style={styles.header}>
+				<HeaderLogPage />
+			</View>
+			<View style={styles.body}>
+				<View style={styles.LogInBorder}>
+					<Text h4 style={styles.bodyText}>
+						Password Reset
+					</Text>
+					<Text style={styles.bodyText}>Create your new password:</Text>
+					<View style={styles.inputView}>
+						<TextInput
+							style={styles.TextInput}
+							placeholder="UserName"
+							placeholderTextColor="#1722e8"
+						/>
+					</View>
+					<View style={styles.inputView}>
+						<TextInput
+							style={styles.TextInput}
+							placeholder="New Password."
+							placeholderTextColor="#1722e8"
+							secureTextEntry={true}
+							onChangeText={(password) => setPassword(password)}
+						/>
+					</View>
+					<View style={styles.inputView}>
+						<TextInput
+							style={styles.TextInput}
+							placeholder="Confirm New Password."
+							placeholderTextColor="#1722e8"
+							secureTextEntry={true}
+							onChangeText={(password) => setPassword(password)}
+						/>
+					</View>
 
-						<View style={styles.body}>
-							<Text style={styles.text}>
-								Open up App.js to start working on your app!
-							</Text>
-						</View>
-
-						<View>
-							<Footer />
-						</View>
-					</ScrollView>
+					<TouchableOpacity style={styles.loginBtn}>
+						<Text style={styles.bodyText}>Submit</Text>
+					</TouchableOpacity>
 				</View>
-			);
-		}
-		const styles = StyleSheet.create({
-			container: {
-				flex: 1,
-				backgroundColor: "#fff",
-			},
-			body: {
-				backgroundColor: "#fff",
-				height: "80%",
-				width: "100%",
-				textAlign: "center",
-				justifyContent: "center",
-			},
-			text: {
-				margin: 5,
-				fontSize: "1.5em",
-				fontWeight: 100,
-				fontFamily: "Serif",
-			},
-		});
+				<Text onPress={() => navigation.goBack()}>Back to Logging Page</Text>
+			</View>
+			<View style={styles.footer}>
+				<Footer />
+			</View>
+			<View />
+		</View>
+	);
+}
 
-		export default RestPassWordPage;
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	body: {
+		backgroundColor: "#fff",
+		height: "80%",
+		width: "100%",
+		textAlign: "center",
+		justifyContent: "center",
+		flexDirection: "row",
+		margin: 20,
+		padding: 40,
+	},
+	bodyText: {
+		color: "#1722e8",
+	},
+	LogInBorder: {
+		border: "solid",
+		borderRadius: 50,
+		backgroundColor: "white",
+		width: "60%",
+		height: "90%",
+		color: "#e9e9f5",
+		alignItems: "center",
+		justifyContent: "space-evenly",
+		marginHorizontal: 10,
+		paddingTop: 20,
+	},
 
+	header: {
+		width: "100%",
+		borderRadius: 10,
+		height: 50,
+		alignItems: "center",
+		justifyContent: "center",
+		marginTop: 40,
+	},
+	footer: {
+		width: "100%",
+		borderRadius: 10,
+		height: 50,
+		alignItems: "center",
+		justifyContent: "center",
+		marginTop: 40,
+	},
+	inputView: {
+		width: "60%",
+		borderRadius: 10,
+		height: 50,
+		alignItems: "center",
+		justifyContent: "center",
+		marginTop: 40,
+		backgroundColor: "#e9e9f5",
+	},
+
+	TextInput: {
+		height: 50,
+		flex: 1,
+		padding: 10,
+		marginLeft: 20,
+	},
+	loginBtn: {
+		width: "20%",
+		borderRadius: 25,
+		height: 50,
+		alignItems: "center",
+		justifyContent: "center",
+		marginTop: 40,
+		backgroundColor: "lightskyblue",
+	},
+});
+export default RestPasswordPage;
