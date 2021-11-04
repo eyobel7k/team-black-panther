3d6c694d5faee6b8865f303768a58d5c6999347f
Eyobel Legese <eyobel7k@gmail.com>
2021-11-03 18:03:57 -0700

    updated message.js

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,38 +1,80 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { ThemeLoggedIn, Chat } from './';
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { ThemeLoggedIn, Chat } from "./";
+import { WPAPI_PATHS, wpApiFetch } from "../services/WPAPI";
 
 function Messages({ navigation }) {
-	return (
-		<ThemeLoggedIn navigation={navigation}>
-			<View style={styles.container}>
-					<View style={styles.body}>
-						<Text style={styles.text}>
-							<Chat />
-						</Text>
-					</View>
-			</View>
-		</ThemeLoggedIn>
-	);
+  const [members, setMembers] = useState([]);
+  useEffect(() => {
+    wpApiFetch({ path: WPAPI_PATHS.buddypress.members })
+      .then((data) => setMembers(data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  console.log("Members", members);
+
+  const listMembers = members.map((member, index) => (
+    <View key={index} style={styles.inlineProfile}>
+      <Image style={styles.image} source={member.avatar_urls.thumb} />
+      <Text>{member.name}</Text>
+    </View>
+  ));
+
+  return (
+    <ThemeLoggedIn navigation={navigation}>
+      <View style={styles.container}>
+        <View style={styles.body}>
+          <Text style={styles.text}>
+            <Chat />
+			
+          </Text>
+		 
+        </View>
+		{listMembers}
+      </View>
+	  
+    </ThemeLoggedIn>
+  );
 }
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-	},
-	body: {
-		backgroundColor: "#fff",
-		height: "80%",
-		width: "100%",
-		textAlign: "center",
-		justifyContent: "center",
-	},
-	text: {
-		margin: 5,
-		fontSize: "1.5em",
-		fontWeight: 100,
-		fontFamily: "Serif",
-	},
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  body: {
+    backgroundColor: "#fff",
+    height: "80%",
+    width: "100%",
+    textAlign: "center",
+    justifyContent: "center",
+  },
+  text: {
+    margin: 5,
+    fontSize: "1.5em",
+    fontWeight: 100,
+    fontFamily: "Serif",
+  },
+
+  image: {
+    width: 50,
+    height: 50,
+    marginRight: 50,
+	alignItems:'flex-end',
+	
+  },
+  inlineProfile: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flext-center',
+    marginBottom: 5,
+  },
+
 });
 
 export default Messages;
+
+
+  
+	
+  
