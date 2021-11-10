ebefcc86ac3be6a97d9b1e2058bdab4daf687dc9
alberm <albertmartinez16@gmail.com>
2021-11-10 09:09:03 -0800

    improve auth, add demo code, add img

--- src/components/LogInPage.js
+++ src/components/LogInPage.js
@@ -9,13 +9,14 @@ import {
 } from "react-native";
 import ThemeLoggedOut from "./ThemeLoggedOut";
 import { Text } from "react-native-elements";
+import welcomeImg from '../../assets/marvelspace_login.png';
 
 	// example response
 	// {
-	// 	"token": "string of random characters",
+	// 	"token": "random.characters.string",
 	// 	"user_display_name": "Iron Man",
-	// 	"user_email": "albertm@alphaworks.tech",
-	// 	"user_nicename": "ironmann",
+	// 	"user_email": "me@marvelspace.com",
+	// 	"user_nickname": "ironmann",
 	// }
 
 function LogInPage({ navigation }) {
@@ -28,6 +29,17 @@ function LogInPage({ navigation }) {
 
 	useEffect(() => {
 		if (loading) {
+			// for demo purposes, will delete afterwards
+			if (username === 'user' && password === 'pass') {
+				new Promise(resolve => setTimeout(() => resolve({
+					"token": "random.characters.string",
+					"user_display_name": "Iron Man",
+					"user_email": "me@marvelspace.com",
+					"user_nickname": "ironmann",
+				}), 2000))
+				.then(formSuccess);
+				return;
+			}
 			const options = {
 				method: "POST",
 				headers: {
@@ -43,7 +55,7 @@ function LogInPage({ navigation }) {
 				`https://jualuc1.dreamhosters.com/wp-json/jwt-auth/v1/token`,
 				options
 			)
-				.then((response) => response.json())
+				.then(response => response.json())
 				.then((data) => {
 					data.token ? formSuccess(data) : formError(data);
 				});
@@ -55,21 +67,20 @@ function LogInPage({ navigation }) {
 		setLoading(true);
 	};
 
-	const formSuccess = (data) => {
-		setToken(data.token);
+	const formSuccess = (response) => {
+		setToken(response.token);
 		setLoggedIn(true);
 		setLoading(false);
 		setUsername("");
 		setPassword("");
-		navigation.navigate("Newsfeed");
+		navigation.navigate("Newsfeed", { ...response });
 	};
 
-	const formError = (data) => {
+	const formError = (response) => {
 		const regex = /<[^>]*>/g;
 		setLoading(false);
 		setPassword("");
-		// data?.message ? setError(data.message.replaceAll(regex, "")) : "";
-		console.log(data);
+		setError(response.message ? response.message.replace(regex, "") : "An error has occurred!");
 	};
 	return (
 		<ThemeLoggedOut navigation={navigation}>
@@ -88,8 +99,8 @@ function LogInPage({ navigation }) {
 					</Text>
 					<View style={styles.imageContainer}>
 						<Image
-							source={{ uri: "https://i.pravatar.cc/300" }}
-							style={{ height: "10%", width: "100%" }}
+							source={welcomeImg}
+							style={{ height: 350, width: 350  }}
 						/>
 					</View>
 				</View>
@@ -112,8 +123,8 @@ function LogInPage({ navigation }) {
 									onChangeText={setUsername}
 									onSubmitEditing={onFormSubmit}
 								/>
-								</View>
-								<View style={styles.inputView}>
+							</View>
+							<View style={styles.inputView}>
 								<Text>Password:</Text>
 								<TextInput
 									// style={styles.input}
@@ -122,8 +133,8 @@ function LogInPage({ navigation }) {
 									onSubmitEditing={onFormSubmit}
 									secureTextEntry={true}
 								/>
-								</View>
-								<TouchableOpacity>
+							</View>
+							<TouchableOpacity>
 								<Button onPress={onFormSubmit} title="Login" />
 								<Text>{loading && "Loading"}</Text>
 								<Text>{error}</Text>
@@ -168,41 +179,47 @@ const styles = StyleSheet.create({
 		justifyContent: "center",
 	},
 	body: {
+		flex: 1,
 		backgroundColor: "#fff",
-		height: "70%",
+		// height: "70%",
 		width: "100%",
-		textAlign: "center",
-		justifyContent: "center",
-		flexDirection: "row",
-		margin: 20,
-		padding: 40,
+		// textAlign: "center",
+		justifyContent: "space-around",
+		flexDirection: "column",
+		// margin: 20,
+		// padding: 40,
 	},
 	bodyText: {
 		color: "#1722e8",
+		alignSelf: 'center'
 	},
 	LogInBorder: {
+		borderWidth: 2,
 		borderStyle: "solid",
 		borderRadius: 50,
 		backgroundColor: "#fff",
-		width: "50%",
-		height: "100%",
+		// width: "50%",
+		// height: "100%",
 		color: "#e9e9f5",
-		alignItems: "center",
-		justifyContent: "space-evenly",
+		// alignItems: "center",
+		// justifyContent: "space-around",
 		marginHorizontal: 10,
 		paddingTop: 20,
 		paddingBottom: 10,
 	},
 	ImageBorder: {
 		// border: "solid",
+		flex: 1,
+		borderWidth: 2,
 		borderRadius: 50,
 		backgroundColor: "#fff",
-		width: "40%",
-		height: "80%",
+		// width: "40%",
+		// height: "80%",
 
 		alignItems: "center",
-		justifyContent: "center",
-		marginHorizontal: 20,
+		// flexWrap: "wrap",
+		// justifyContent: "center",
+		marginHorizontal: 10,
 	},
 	header: {
 		width: "100%",
@@ -221,7 +238,7 @@ const styles = StyleSheet.create({
 		marginTop: 40,
 	},
 	inputView: {
-		width: "60%",
+		// width: "60%",
 		borderRadius: 10,
 		height: 50,
 		alignItems: "center",
@@ -235,7 +252,7 @@ const styles = StyleSheet.create({
 		flex: 1,
 		padding: 10,
 		marginLeft: 20,
-		alignItems: "center",
+		// alignItems: "center",
 	},
 
 	forgot_button: {
