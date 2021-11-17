66b05ee25b88c6aac4ec9940825c0e03e865cfd0
Hiwot-Beshe <beshehiwot@gmail.com>
2021-11-16 17:25:33 -0800

    add members list and header change friends to Members  head title

--- src/components/Friends.js
+++ src/components/Friends.js
@@ -1,13 +1,51 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View } from "react-native";
 import ThemeLoggedIn from "./ThemeLoggedIn";
+import { WPAPI_PATHS, wpApiFetch } from "../services/WPAPI";
+import { Text, ListItem, Avatar } from "react-native-elements";
 
 function Friends({ navigation }) {
+	const [members, setMembers] = useState([]);
+	const [selectedMember, setSelectedMember] = useState({});
+	const onPress = (selectedMemberId) => {
+		setSelectedMember(members[selectedMemberId]);
+	};
+
+	useEffect(() => {
+		wpApiFetch({ path: WPAPI_PATHS.buddypress.members })
+			.then((data) => {
+				setMembers(data);
+				console.log(data);
+			})
+
+			.catch((error) => console.log(error));
+	}, []);
+
 	return (
 		<ThemeLoggedIn navigation={navigation}>
-			<View style={styles.container} navigation={navigation}>
+			<View style={styles.container}>
+				<View style={styles.title}>
+					<Text h4> Members</Text>
+				</View>
 				<View style={styles.body}>
-					<Text style={styles.text}>Friends!</Text>
+					{members.map((member, index) => (
+						<ListItem
+							label={member.name}
+							key={index}
+							value={member.name}
+							bottomDivider
+						>
+							<Avatar source={{ uri: member.avatar_urls.full }} />
+
+							<ListItem.Content>
+								<ListItem.Title>
+									{member.name}
+									{member.thumb}
+								</ListItem.Title>
+							</ListItem.Content>
+							<ListItem.Chevron color="white" />
+						</ListItem>
+					))}
 				</View>
 			</View>
 		</ThemeLoggedIn>
@@ -20,16 +58,22 @@ const styles = StyleSheet.create({
 	},
 	body: {
 		backgroundColor: "#efd595",
-		height: "80%",
-		width: "100%",
+		// height: "10%",
+		width: "60%",
 		textAlign: "center",
 		justifyContent: "center",
+		marginHorizontal: "20%",
+		// alignItems: "center",
 	},
 	text: {
 		margin: 5,
-		fontSize: 10,
-		fontWeight: "100",
+		fontSize: 14,
+		fontWeight: "bold",
 		// fontFamily: "Serif",
+	},
+	title: {
+		alignItems: "center",
+		justifyContent: "center",
 	},
 });
 
