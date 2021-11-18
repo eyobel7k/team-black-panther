0514ceaea7c5ad2ecf9b0b0d4cf6e7af5257b92a
Hiwot-Beshe <beshehiwot@gmail.com>
2021-11-18 09:59:53 -0800

    change style on Image page

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,121 +1,121 @@
 import ThemeLoggedIn from "./ThemeLoggedIn";
 import React, { useState, useEffect } from "react";
 import {
-  StyleSheet,
-  Text,
-  View,
-  Button,
-  Image,
-  ScrollView,
+	StyleSheet,
+	Text,
+	View,
+	Button,
+	Image,
+	ScrollView,
 } from "react-native";
 import { WPAPI_PATHS, wpApiFetch } from "../services/WPAPI";
 import { MaterialIcons } from "@expo/vector-icons";
 
 export default function Images({ navigation }) {
-  const [imageArr, setImageArr] = useState([]);
-  useEffect(() => {
-    wpApiFetch({ path: WPAPI_PATHS.wp.media }).then((data) =>
-      setImageArr(data)
-    );
-  }, []);
-  const [imageInput, setImageInput] = useState("");
+	const [imageArr, setImageArr] = useState([]);
+	useEffect(() => {
+		wpApiFetch({ path: WPAPI_PATHS.wp.media }).then((data) =>
+			setImageArr(data)
+		);
+	}, []);
+	const [imageInput, setImageInput] = useState("");
 
-  const uploadImage = () => {
-    // wordpress API
-  };
+	const uploadImage = () => {
+		// wordpress API
+	};
 
-  const sendImage = () => {
-    setImageArr([...imageArr, { image: imageInput }]);
-    setImageInput("");
-  };
+	const sendImage = () => {
+		setImageArr([...imageArr, { image: imageInput }]);
+		setImageInput("");
+	};
 
-  const deleteImage = (index) => {
-    setImageArr(imageArr.filter((image, selected) => selected != index));
-  };
+	const deleteImage = (index) => {
+		setImageArr(imageArr.filter((image, selected) => selected != index));
+	};
 
-  const generateGallery = imageArr.map((img, index) => {
-    // imgWidth: 40vh
-    // const imgWidth = document.documentElement.clientHeight * 0.40;
-    const imgWidth = 250;
-    const imgHeight =
-      (img.media_details.height / img.media_details.width) * imgWidth;
+	const generateGallery = imageArr.map((img, index) => {
+		// imgWidth: 40vh
+		// const imgWidth = document.documentElement.clientHeight * 0.40;
+		const imgWidth = 250;
+		const imgHeight =
+			(img.media_details.height / img.media_details.width) * imgWidth;
 
-    return (
-      <View key={index}>
-        <View style={styles.imageRow} key={index}>
-          <Image
-            style={{ width: imgWidth, height: imgHeight }}
-            source={{ uri: img.source_url }}
-          />
-        </View>
-        <View style={styles.deleteButton}>
-          <Button
-            color="#5569FE"
-            key={index}
-            onPress={() => deleteImage(index)}
-            title="delete"
-          />
-          <View></View>
-        </View>
-      </View>
-    );
-  });
+		return (
+			<View key={index}>
+				<View style={styles.imageRow} key={index}>
+					<Image
+						style={{ width: imgWidth, height: imgHeight }}
+						source={{ uri: img.source_url }}
+					/>
+				</View>
+				<View style={styles.deleteButton}>
+					<Button
+						color="#87cefa"
+						key={index}
+						onPress={() => deleteImage(index)}
+						title="delete"
+					/>
+					<View></View>
+				</View>
+			</View>
+		);
+	});
 
-  return (
-    <ThemeLoggedIn navigation={navigation}>
-      <ScrollView style={styles.background}>
-        <View style={styles.topContainer}>
-          <View style={styles.buttonContainer}>
-            {/* <Button color="#5569FE" onPress={uploadImage} title="Upload" /> */}
-            <View style={styles.spacing} />
-            {/* <Button color="#5569FE" onPress={sendImage} title="Send" /> */}
-          </View>
-        </View>
+	return (
+		<ThemeLoggedIn navigation={navigation}>
+			<ScrollView style={styles.background}>
+				<View style={styles.topContainer}>
+					<View style={styles.buttonContainer}>
+						{/* <Button color="#5569FE" onPress={uploadImage} title="Upload" /> */}
+						<View style={styles.spacing} />
+						{/* <Button color="#5569FE" onPress={sendImage} title="Send" /> */}
+					</View>
+				</View>
 
-        <View style={styles.imageContainer}>{generateGallery}</View>
-        <Button color="#5569FE" onPress={uploadImage} title="Upload" />
-        <Button color="#5569FE" onPress={sendImage} title="Send" />
-      </ScrollView>
-    </ThemeLoggedIn>
-  );
+				<View style={styles.imageContainer}>{generateGallery}</View>
+				<Button color="#87cefa" onPress={uploadImage} title="Upload" />
+				<Button color="#87cefa" onPress={sendImage} title="Send" />
+			</ScrollView>
+		</ThemeLoggedIn>
+	);
 }
 
 const styles = StyleSheet.create({
-  background: {
-    backgroundColor: "#fff",
-  },
-  topContainer: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  image: {
-    minHeight: "100%",
-    minWidth: "100%",
-  },
-  imageContainer: {
-    backgroundColor: "#8291CE",
-    display: "flex",
-    // flexWrap: 'wrap',
-    // flexDirection: 'row',
-    margin: 10,
-  },
-  imageRow: {
-    height: "auto",
-    flexGrow: 1,
-    alignItems: "center",
-  },
-  buttonContainer: {
-    flexDirection: "row",
-  },
-  deleteButton: {
-    width: "20%",
-    alignSelf: "center",
-    margin: 5,
-  },
-  spacing: {
-    width: 5,
-  },
+	background: {
+		backgroundColor: "#efd595",
+	},
+	topContainer: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	image: {
+		minHeight: "100%",
+		minWidth: "100%",
+	},
+	imageContainer: {
+		backgroundColor: "#c5834c",
+		display: "flex",
+		// flexWrap: 'wrap',
+		// flexDirection: 'row',
+		margin: 10,
+	},
+	imageRow: {
+		height: "auto",
+		flexGrow: 1,
+		alignItems: "center",
+	},
+	buttonContainer: {
+		flexDirection: "row",
+	},
+	deleteButton: {
+		width: "20%",
+		alignSelf: "center",
+		margin: 5,
+	},
+	spacing: {
+		width: 5,
+	},
 });
 
 // const styles = StyleSheet.create({
